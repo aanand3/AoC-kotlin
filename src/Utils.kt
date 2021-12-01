1642231530e9b97ed3589437ce71c9d9d6c8bada
aanand3 <as5265@columbia.edu>
2021-12-01 10:57:49 -0600

    day 1 solution

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,8 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Converts List of String to List of Int
+ */
+fun listToInt(input: List<String>) = input.map { it.toInt() }
